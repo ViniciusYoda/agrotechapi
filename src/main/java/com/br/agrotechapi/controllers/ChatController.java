fb037c6ce507296f11ca71dd977dd9acae753e3a
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-27 12:10:05 -0300

    api

--- src/main/java/com/br/agrotechapi/controllers/ChatController.java
+++ src/main/java/com/br/agrotechapi/controllers/ChatController.java
@@ -1,27 +1,50 @@
 package com.br.agrotechapi.controllers;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import com.br.agrotechapi.service.ChatService;
-
-@RestController
-@RequestMapping("/api/chat")
+@PostMapping("/ask")
 public class ChatController {
-    
-    @Autowired
-    private ChatService chatService;
+public String askQuestion(@RequestBody Map<String, String> requestData) {
 
-    @PostMapping
-    public ResponseEntity<String> chatWithBot(@RequestBody Map<String, String> requestBody) {
-        String userMessage = requestBody.get("message");
-        String chatbotResponse = chatService.getChatResponse(userMessage);
-        return ResponseEntity.ok(chatbotResponse);
-    }
+    String pergunta = requestData.get("quest");
+    String quest = INTRO_TEXT + " " + pergunta;
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.setBearerAuth(API_KEY);
+
+    Map<String, Object> requestBody = new LinkedHashMap<>();
+    requestBody.put("model", "gpt-3.5-turbo");
+    List<Map<String, String>> messages = new ArrayList<>();
+    Map<String, String> message = new HashMap<>();
+    message.put("role", "user");
+    message.put("content", quest);
+    messages.add(message);
+    requestBody.put("messages", messages);
+
+    HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+
+    ResponseEntity<String> response = restTemplate.postForEntity(
+            "https://api.openai.com/v1/chat/completions",
+            entity,
+            String.class
+    );
+
+    String responseBody = response.getBody();
+
+    // Process responseBody as needed
+    
+    return responseBody;
 }
+}
+
--- src/main/java/com/br/agrotechapi/service/ChatService.java
+++ src/main/java/com/br/agrotechapi/service/ChatService.java
@@ -1,69 +1,69 @@
-package com.br.agrotechapi.service;
+// package com.br.agrotechapi.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.core.env.Environment;
+// import org.springframework.http.HttpEntity;
+// import org.springframework.http.HttpHeaders;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.http.MediaType;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.Collections;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
 
-@Service
-public class ChatService {
+// @Service
+// public class ChatService {
 
-    @Autowired
-    private Environment environment;
+//     @Autowired
+//     private Environment environment;
 
-    private List<String> agricultureKeywords = Arrays.asList("cultivar", "plantação", "colheita", "solo", "irrigação", "clima");
+//     private List<String> agricultureKeywords = Arrays.asList("cultivar", "plantação", "colheita", "solo", "irrigação", "clima");
 
-    public String getApiKey() {
-        return environment.getProperty("GPT3_API_KEY");
-    }
+//     public String getApiKey() {
+//         return environment.getProperty("GPT3_API_KEY");
+//     }
 
-    public String getChatResponse(String userMessage) {
-        String apiKey = getApiKey();
+//     public String getChatResponse(String userMessage) {
+//         String apiKey = getApiKey();
 
-        String apiUrl = "https://api.openai.com/v1/chat/completions";
+//         String apiUrl = "https://api.openai.com/v1/chat/completions";
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + apiKey);
-        headers.setContentType(MediaType.APPLICATION_JSON);
+//         RestTemplate restTemplate = new RestTemplate();
+//         HttpHeaders headers = new HttpHeaders();
+//         headers.set("Authorization", "Bearer " + apiKey);
+//         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        boolean isAgricultureRelated = agricultureKeywords.stream()
-                .anyMatch(keyword -> userMessage.toLowerCase().contains(keyword));
+//         boolean isAgricultureRelated = agricultureKeywords.stream()
+//                 .anyMatch(keyword -> userMessage.toLowerCase().contains(keyword));
 
-        if (isAgricultureRelated) {
-            Map<String, Object> systemMessage = Collections.singletonMap("role", "system");
-            Map<String, Object> userMessageMap = new HashMap<>();
-            userMessageMap.put("role", "user");
-            userMessageMap.put("content", userMessage);
+//         if (isAgricultureRelated) {
+//             Map<String, Object> systemMessage = Collections.singletonMap("role", "system");
+//             Map<String, Object> userMessageMap = new HashMap<>();
+//             userMessageMap.put("role", "user");
+//             userMessageMap.put("content", userMessage);
 
-            List<Map<String, Object>> messages = new ArrayList<>();
-            messages.add(systemMessage);
-            messages.add(userMessageMap);
+//             List<Map<String, Object>> messages = new ArrayList<>();
+//             messages.add(systemMessage);
+//             messages.add(userMessageMap);
 
-            Map<String, Object> requestBody = new HashMap<>();
-            requestBody.put("messages", messages);
+//             Map<String, Object> requestBody = new HashMap<>();
+//             requestBody.put("messages", messages);
 
-            HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
-            ResponseEntity<Map> response = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, Map.class);
+//             HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+//             ResponseEntity<Map> response = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, Map.class);
 
-            String chatResponse = (String) ((List<Map<String, Object>>) response.getBody().get("choices")).get(0).get("message");
+//             String chatResponse = (String) ((List<Map<String, Object>>) response.getBody().get("choices")).get(0).get("message");
 
-            return chatResponse;
-        } else {
-            return "Desculpe, só consigo responder a perguntas relacionadas à agricultura.";
-        }
-    }
-}
+//             return chatResponse;
+//         } else {
+//             return "Desculpe, só consigo responder a perguntas relacionadas à agricultura.";
+//         }
+//     }
+// }
 
